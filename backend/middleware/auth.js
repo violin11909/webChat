0013a70042fa6cadc20bf047ea28689849822c27
Thanapat Aupprathumwipanon <Thanapat_petch@hotmail.com>
2025-11-06 22:56:57 +0700

    initialize backend folder

---
+++ backend/middleware/auth.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken')
+const User = require("../models/User")
+
+// Protect routes
+exports.protect = async (req, res, next) => {
+    let token
+    
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        // split and select 2nd element
+        token = req.headers.authorization.split(' ')[1]
+    }
+
+    // Make sure token exists
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: 'Not authorize to access this route'
+        })
+    }
+
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // console.log(decoded)
+
+        req.user = await User.findById(decoded.id)
+
+        next()
+    } catch (error) {
+        console.log(error.stack)
+        return res.status(401).json({
+            success: false,
+            message: 'Not authorize to access this route'
+        })
+    }
+}
